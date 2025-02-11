29ebf1e772719fc09fe64b595adf197148d9322c
mosaicmind26 <chitrakamboj06@gmail.com>
2025-02-11 14:06:22 +0530

    Solution to Grade Evaluation

--- Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
+++ Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
@@ -8,7 +8,7 @@ int main() {
     //printf("%s", welcome());
     char letter;
     scanf(%c,&letter);
-    
+    char upper = toupper(letter);
     if (letter==A){
         printf("Excellent");
     }
